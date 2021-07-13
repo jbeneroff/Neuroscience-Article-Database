69e23f8769a01444ef33c42c2ebab01ba4c6e017
Jared Beneroff <jaredbeneroff@Jareds-MacBook-Pro.local>
2021-07-13 16:33:37 -0400

    basic components

---
+++ src/services/index.js
@@ -0,0 +1,4 @@
+export const AIRTABLE_KEY = process.env.REACT_APP_AIRTABLE_KEY
+export const AIRTABLE_BASE = process.env.REACT_APP_AIRTABLE_BASE
+export const BASE_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE}/articles`
+export const headers = { Authorization: `Bearer ${AIRTABLE_KEY}` }
